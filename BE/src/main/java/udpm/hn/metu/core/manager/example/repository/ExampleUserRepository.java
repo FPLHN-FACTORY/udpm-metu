5ed225b5589bc801e3098392c9235493b1b5e430
Oniamey <nghiabe.dev@gmail.com>
2024-10-04 11:54:29 +0700

    config: databaseGenegator (For Security)

--- BE/src/main/java/udpm/hn/metu/core/manager/example/repository/ExampleUserRepository.java
+++ BE/src/main/java/udpm/hn/metu/core/manager/example/repository/ExampleUserRepository.java
@@ -1,11 +1,13 @@
 package udpm.hn.metu.core.manager.example.repository;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import udpm.hn.metu.core.manager.example.model.ExampleUserResponse;
 import udpm.hn.metu.repository.UserRepository;
 
 import java.util.List;
 
+@Repository
 public interface ExampleUserRepository extends UserRepository {
 
     @Query(value = """
---
+++ BE/src/main/java/udpm/hn/metu/infrastructure/config/repository/DBGenBusinessTypeRepository.java
@@ -0,0 +1,11 @@
+package udpm.hn.metu.infrastructure.config.repository;
+
+import org.springframework.stereotype.Repository;
+import udpm.hn.metu.repository.BusinessTypeRepository;
+
+@Repository
+public interface DBGenBusinessTypeRepository extends BusinessTypeRepository {
+
+
+
+}
